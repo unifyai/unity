83601c24f80c403779ea0733e96946d37527fbf8
djl11 <dan@unify.ai>
2025-04-17 23:15:39 -0700

    major updates.

--- sys_msgs.py
+++ sys_msgs.py
@@ -1,11 +1,11 @@
-import os
+INTERJECTION_TO_BROWSER_ACTION = """
+Your task is to take a plain English requests provided by the user, and then select the most appropriate action to take along with your reasoning for why each action should and should not be taken.
 
-from dotenv import load_dotenv
+In cases where you deem the reasoning to be very self-evident, you can leave the field blank.
 
-load_dotenv()
+In cases where it's a non-trivial decision for a candidate action, then you should state the reasoning behind your decision in detail.
 
-FIRST_NAME = os.environ["FIRST_NAME"]
+The full set of available actions is provided in the response schema you've been provided. Please respond `True` in the `apply` field to all actions which you think would achieve the user's request, if applied in isolation.
 
-vocal_request_taker_sys_msg = f"You are a general assistant, able to perform ANY task on behalf of {FIRST_NAME}. You are able to log into their LinkedIn account, check their GMail, make calls to their friend, and you are able to do pretty much anything else you can think of. Please NEVER tell {FIRST_NAME} that you are unable to complete a task. Your goal on this call is to simply listen to the requests made by {FIRST_NAME}, answer any questions that you can confidently answer, and ask any clarifying questions for the tasks requested by {FIRST_NAME}, only if needed. Please be brief, succinct and fast with your responses, {FIRST_NAME} is very busy. You do not need to *perform* any of the requested tasks during the call. They will be completed afterwards."
-
-extract_request_from_call_sys_msg = f"You will be given the full transcript for a conversation, as well as a short summary of the conversation. Within this conversation, {FIRST_NAME} has requested for one or more tasks to be completed. Your task is to extract this task or list of tasks, and present them as a list. Each task such be very clearly articulated, and should only include relevant information for that specific task."
+You must respond True to at least one action, and you cannot respond True to all of them.
+"""
