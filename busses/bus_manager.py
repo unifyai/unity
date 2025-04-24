8f3dbd67f2c979b02078afaa11dede32e8f3b3e8
djl11 <dan@unify.ai>
2025-04-24 13:19:48 -0700

    created Planner class, which for now acts as a simple passthrough to the low-level text commands.

--- busses/bus_manager.py
+++ busses/bus_manager.py
@@ -2,6 +2,7 @@ import queue
 import asyncio
 from task_managers.task_manager import TaskManager
 from controller.controller import Controller
+from planner.planner import Planner
 
 
 class BusManager:
@@ -14,8 +15,11 @@ class BusManager:
         # the latest (windowed) user-agent transcript, updated after every new exchange
         self._transcript_q: queue.Queue[list[str]] = queue.Queue()
 
+        # low-level browser actions, in text form
+        self._text_action_q: queue.Queue[str] = queue.Queue()
+
         # user task requests, in text form
-        self._text_command_q: queue.Queue[str] = queue.Queue()
+        self._text_task_q: queue.Queue[str] = queue.Queue()
 
         # lower-level browser commands
         self._browser_command_q: queue.Queue[str] = queue.Queue()
@@ -23,7 +27,10 @@ class BusManager:
         # playwright browser state
         self._browser_state_q: queue.Queue[str] = queue.Queue()
 
-        # tasks which have been completed, by their title
+        # actions which have been completed, referenced by their title
+        self._action_completion_q: queue.Queue[str] = queue.Queue()
+
+        # tasks which have been completed, referenced by their title
         self._task_completion_q: asyncio.Queue[str] = asyncio.Queue()
 
         # Managers #
@@ -31,24 +38,39 @@ class BusManager:
 
         # re-organizes and schedules task, based on transcripts
         self._task_manager = TaskManager(
+            # [reads from]: detect when a task trigger + change is requested from transcript
             self._transcript_q,
-            self._text_command_q,
+            # [writes to]: parses intent from transcript + sends clear text commands
+            self._text_task_q,
         )
 
         # handles hierarchical task planning + decomposition
-        # ToDo
-        # include self._task_completion_q,
-        # self._planner = ...
+        self._planner = Planner(
+            # [read from]: take high-level text-based tasks and decomposes into low-level text-based actions
+            self._text_task_q,
+            # [writes to]: send these low-level text-based actions to the controller
+            self._text_action_q,
+            # [reads from]: determines when the low-level actions are completed
+            self._action_completion_q,
+            # [writes to]: writes incremental task progress, so the user-facing assistant stays updated
+            self._task_completion_q,
+        )
 
         # handles text -> low-level browser commands
         self._controller = Controller(
-            self._text_command_q,
+            # [reads from]: take low-level text-based actions and convert to browser actions
+            self._text_action_q,
+            # [reads from]: use the browser state as context for text->action controller
             self._browser_state_q,
+            # [writes to]: send the browser commands for the browser worker to execute
             self._browser_command_q,
+            # [writes to]: sends the name of the completed action, once it is completed
+            self._action_completion_q,
         )
 
     def start(self):
         self._task_manager.start()
+        self._planner.start()
         self._controller.start()
 
     # Properties #
