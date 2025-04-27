10444fba89667899a76e706a65711ce3efbe43b8
djl11 <dan@unify.ai>
2025-04-26 20:32:32 -0700

    added _handle_project decorator, and fixed tests.

--- tests/test_communication/test_message.py
+++ tests/test_communication/test_message.py
@@ -3,18 +3,18 @@ from datetime import datetime
 
 
 def test_message():
-    timestamp = datetime.now()
+    timestamp = datetime.now().isoformat()
     msg = Message(
         medium="email",
-        sender="Daniel Lenton",
-        receiver="My Assistant",
+        sender_id=0,
+        receiver_id=1,
         timestamp=timestamp,
         content="Hello, how are you?",
         exchange_id=0,
     )
     assert msg.medium == "email"
-    assert msg.sender == "Daniel Lenton"
-    assert msg.receiver == "My Assistant"
+    assert msg.sender_id == 0
+    assert msg.receiver_id == 1
     assert msg.timestamp == timestamp
     assert msg.content == "Hello, how are you?"
     assert msg.exchange_id == 0
