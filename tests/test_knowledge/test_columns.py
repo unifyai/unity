f6c97a720900d5033c433dcd41ceb2868752a377
djl11 <dan@unify.ai>
2025-05-02 15:34:58 -0700

    added test_create_derived_column, not yet passing.

--- tests/test_knowledge/test_columns.py
+++ tests/test_knowledge/test_columns.py
@@ -10,3 +10,24 @@ def test_create_empty_column():
     knowledge_manager._create_empty_column("MyTable", "MyCol", "int")
     tables = knowledge_manager._list_tables(include_columns=True)
     assert tables == {"MyTable": {"description": None, "columns": {"MyCol": "int"}}}
+
+
+@_handle_project
+def test_create_derived_column():
+    knowledge_manager = KnowledgeManager()
+    knowledge_manager.start()
+    knowledge_manager._create_table("MyTable")
+    knowledge_manager._add_data("MyTable", [{"x": 1, "y": 2}, {"x": 3, "y": 4}])
+    knowledge_manager._create_derived_column(
+        "MyTable",
+        "distance",
+        "float",
+        "({x}**2 + {y}**2)**0.5",
+    )
+    data = knowledge_manager._search()
+    assert data == {
+        "MyTable": [
+            {"x": 1, "y": 2, "distance": (1**2 + 2**2) ** 0.5},
+            {"x": 3, "y": 4, "distance": (3**2 + 4**2) ** 0.5},
+        ],
+    }
