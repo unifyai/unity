6c856a91795adcbae96743b1f9ac9f9a49c10fd2
djl11 <dan@unify.ai>
2025-05-04 00:30:40 -0700

    made a start on TaskListManager.

---
+++ task_list_manager/sys_msgs.py
@@ -0,0 +1,10 @@
+import json
+from task_list_manager.types.task_list import TaskList
+
+UPDATE = f"""
+Your task is to update the list of tasks based on the plain-text request from the user, and you should continue using the tools available until you are satisfied that the list of tasks has been updated correctly.
+
+As a recap, the schema for the table which stores the list of tasks is as follows:
+
+{json.dumps(TaskList.model_json_schema(), indent=4)}
+"""
