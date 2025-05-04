86fcbe2df4a6a7c2c79f9ef1485fb3f82d4f48f8
djl11 <dan@unify.ai>
2025-05-04 13:00:59 -0700

    refactor test_task_list into two test files.

--- tests/test_task_list/test_update_task.py
+++ tests/test_task_list/test_update_task.py
@@ -3,47 +3,6 @@ from task_list_manager.task_list_manager import TaskListManager
 
 
 @_handle_project
-def test_create_task():
-    task_list_manager = TaskListManager()
-    task_list_manager.start()
-    task_list_manager._create_task(
-        name="Promote Jeff Smith",
-        description="Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
-    )
-    task_list = task_list_manager._search()
-    assert task_list == [
-        {
-            "name": "Promote Jeff Smith",
-            "description": "Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
-            "start_at": None,
-            "deadline": None,
-            "repeat": None,
-            "priority": None,
-            "task_id": 0,
-        },
-    ]
-
-
-@_handle_project
-def test_delete_task():
-    task_list_manager = TaskListManager()
-    task_list_manager.start()
-
-    # create
-    task_list_manager._create_task(
-        name="Promote Jeff Smith",
-        description="Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
-    )
-    task_list = task_list_manager._search()
-    assert len(task_list) == 1
-
-    # delete
-    task_list_manager._delete_task(task_id=0)
-    task_list = task_list_manager._search()
-    assert task_list == []
-
-
-@_handle_project
 def test_update_task_name():
     task_list_manager = TaskListManager()
     task_list_manager.start()
