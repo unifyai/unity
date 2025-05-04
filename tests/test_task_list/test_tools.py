903f211830fcf1cde007ed3c733facb58c0d737a
djl11 <dan@unify.ai>
2025-05-04 12:17:16 -0700

    added test_update_task_name, which is passing.

--- tests/test_task_list/test_tools.py
+++ tests/test_task_list/test_tools.py
@@ -35,19 +35,31 @@ def test_delete_task():
         description="Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
     )
     task_list = task_list_manager._search()
-    assert task_list == [
-        {
-            "name": "Promote Jeff Smith",
-            "description": "Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
-            "start_at": None,
-            "deadline": None,
-            "repeat": None,
-            "priority": None,
-            "task_id": 0,
-        },
-    ]
+    assert len(task_list) == 1
 
     # delete
     task_list_manager._delete_task(task_id=0)
     task_list = task_list_manager._search()
     assert task_list == []
+
+
+@_handle_project
+def test_update_task_name():
+    task_list_manager = TaskListManager()
+    task_list_manager.start()
+
+    # create
+    task_list_manager._create_task(
+        name="Promote Jeff Smith",
+        description="Send an email to Jeff Smith, kindly congratulating him and explaining that he has been promoted from sales rep to sales manager.",
+    )
+    task_list = task_list_manager._search()
+    assert task_list[0]["name"] == "Promote Jeff Smith"
+
+    # rename
+    task_list_manager._update_task_name(
+        task_id=0,
+        new_name="Give Jeff Smith a promotion",
+    )
+    task_list = task_list_manager._search()
+    assert task_list[0]["name"] == "Give Jeff Smith a promotion"
