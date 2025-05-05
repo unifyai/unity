2685ab0874a9b25a2fadb86088339248380d58b8
djl11 <dan@unify.ai>
2025-05-04 17:56:45 -0700

    small typo fixes for TaskListManager.

--- task_list_manager/task_list_manager.py
+++ task_list_manager/task_list_manager.py
@@ -12,7 +12,7 @@ class TaskListManager(threading.Thread):
 
     def __init__(self, *, daemon: bool = True) -> None:
         """
-        Responsible managing the list of tasks, updating the names, descriptions, schedles, repeating pattern and status of all tasks.
+        Responsible for managing the list of tasks, updating the names, descriptions, schedules, repeating pattern and status of all tasks.
         """
         super().__init__(daemon=daemon)
         # ToDo: implement the tools
