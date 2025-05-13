028e659eb919d2e714cde46da0e1649bb49a9995
djl11 <dan@unify.ai>
2025-05-13 22:50:03 +0100

    added status to task schema.

--- task_list_manager/types/task.py
+++ task_list_manager/types/task.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional, List
 from task_list_manager.types.priority import Priority
+from task_list_manager.types.status import Status
 
 
 class Task(BaseModel):
     task_id: int
     name: str
     description: str
+    status: Status
     start_at: Optional[str]
     deadline: Optional[str]
     repeat: Optional[List[str]]
