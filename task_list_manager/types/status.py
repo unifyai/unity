54f0937222902fbe9c2077fb32228819b9fb2be5
djl11 <dan@unify.ai>
2025-05-13 23:26:04 +0100

    updated task schema, and added some placeholder methods.

--- task_list_manager/types/status.py
+++ task_list_manager/types/status.py
@@ -4,6 +4,7 @@ from enum import StrEnum
 class Status(StrEnum):
     scheduled = "scheduled"
     queued = "queued"
+    paused = "paused"
     in_progress = "in_progress"
     done = "done"
     cancelled = "cancelled"
