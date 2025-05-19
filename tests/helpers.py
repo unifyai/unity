407e2fb387d135c2c3708c824d38c29b2ea060b3
djl11 <dan@unify.ai>
2025-05-19 10:55:14 +0100

    expanded _handle_project arguments, enabling reuse of old projects (if available) and cleanup on test exit.

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,26 +4,27 @@ import traceback
 import unify
 
 
-def _handle_project(test_fn=None, delete_on_cleanup=False):
+def _handle_project(test_fn=None, try_reuse_prev: bool = False, delete_on_exit=False):
 
     if test_fn is None:
         return lambda f: _handle_project(
             f,
-            delete_on_cleanup=delete_on_cleanup,
+            try_reuse_prev=try_reuse_prev,
+            delete_on_exit=delete_on_exit,
         )
 
     @functools.wraps(test_fn)
     def wrapper(*args, **kwargs):
         project = test_fn.__name__
-        if project in unify.list_projects():
+        if not try_reuse_prev and project in unify.list_projects():
             unify.delete_project(project)
         try:
             with unify.Project(project):
                 unify.traced(test_fn)(*args, **kwargs)
-            if delete_on_cleanup:
+            if delete_on_exit:
                 unify.delete_project(project)
         except:
-            if delete_on_cleanup:
+            if delete_on_exit:
                 unify.delete_project(project)
             exc_type, exc_value, exc_tb = sys.exc_info()
             tb_string = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
