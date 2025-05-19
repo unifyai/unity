93a2d3c4325977791e128b221b664c2258fcd0e1
djl11 <dan@unify.ai>
2025-05-19 11:15:37 +0100

    updated unit tests such that there is a single project, and a different context per test.

--- tests/helpers.py
+++ tests/helpers.py
@@ -4,28 +4,32 @@ import traceback
 import unify
 
 
-def _handle_project(test_fn=None, try_reuse_prev: bool = False, delete_on_exit=False):
+def _handle_project(
+    test_fn=None,
+    try_reuse_prev_ctx: bool = False,
+    delete_ctx_on_exit=False,
+):
 
     if test_fn is None:
         return lambda f: _handle_project(
             f,
-            try_reuse_prev=try_reuse_prev,
-            delete_on_exit=delete_on_exit,
+            try_reuse_prev_ctx=try_reuse_prev_ctx,
+            delete_ctx_on_exit=delete_ctx_on_exit,
         )
 
     @functools.wraps(test_fn)
     def wrapper(*args, **kwargs):
-        project = test_fn.__name__
-        if not try_reuse_prev and project in unify.list_projects():
-            unify.delete_project(project)
+        ctx = test_fn.__name__
+        if not try_reuse_prev_ctx and ctx in unify.get_contexts():
+            unify.delete_context(ctx)
         try:
-            with unify.Project(project):
+            with unify.Context(ctx):
                 unify.traced(test_fn)(*args, **kwargs)
-            if delete_on_exit:
-                unify.delete_project(project)
+            if delete_ctx_on_exit:
+                unify.delete_context(ctx)
         except:
-            if delete_on_exit:
-                unify.delete_project(project)
+            if delete_ctx_on_exit:
+                unify.delete_context(ctx)
             exc_type, exc_value, exc_tb = sys.exc_info()
             tb_string = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
             raise Exception(f"{tb_string}")
