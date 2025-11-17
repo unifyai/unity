fd3ec175c060774e19fe3e258b53eb7fbd8f7715
CatB1t <skytedits@gmail.com>
2025-11-17 08:33:37 +0200

    Simplify code

--- unity/task_scheduler/activation_ops.py
+++ unity/task_scheduler/activation_ops.py
@@ -100,10 +100,7 @@ def detach_from_queue_for_activation(
                 head_start_at = cur_head.schedule_start_at
 
     # Batch-fetch log objects for all relevant task_ids in one backend call (reuse scheduler helper)
-    needed_ids: list[int] = []
-    for _tid in (task_id, prev_tid, next_tid):
-        if _tid is not None:
-            needed_ids.append(_tid)
+    needed_ids: list[int] = [t for t in (task_id, prev_tid, next_tid) if t is not None]
     try:
         log_objs = scheduler._get_logs_by_task_ids(  # type: ignore[attr-defined]
             task_ids=needed_ids,
