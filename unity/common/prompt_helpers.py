7c0ddf4d8cb7ddd6566cf9b93f3ffd9c1c4a73dc
CatB1t <skytedits@gmail.com>
2026-02-04 00:03:33 +0200

    fix: remove automatic newline prefix for new block in PromptParts.add

--- unity/common/prompt_helpers.py
+++ unity/common/prompt_helpers.py
@@ -375,8 +375,7 @@ class PromptParts:
             self._parts[-1]["content"] += joiner + part
         else:
             # Different static - add new block
-            content = ("\n\n" + part) if separator else "\n" + part
-            self._parts.append({"type": "text", "content": content, "static": static})
+            self._parts.append({"type": "text", "content": part, "static": static})
 
     def to_list(self) -> List[Dict[str, Any]]:
         """Return the internal structured parts."""
